481d67d90578f500fb372c6120a84642fc1a8539
beansparrow <stvhunt132@gmail.com>
2021-06-03 17:56:55 -0400

    Added 8 player portraits and healthbars

--- src/game_functions.js
+++ src/game_functions.js
@@ -1,11 +1,24 @@
 function dealDamage(){
-    let health = document.getElementById("health").value
-    let damageValue = document.getElementById("DamageP1").value
-    health.value -= damageValue
+    const health = document.getElementById("health")
+    const damageValue = parseInt(document.getElementById("DamageP1").value)
+    const newHealth = health.value - damageValue
+    health.setAttribute("value", newHealth)
 }
 
+
 function healDamage(){
-    let health = document.getElementById("health").value
-    let healValue = document.getElementById("HealP1").value
-    health.value += healValue
+    const health = document.getElementById("health")
+    const healValue = parseInt(document.getElementById("HealP1").value)
+    const newHealth = health.value + healValue
+    health.setAttribute("value", newHealth)
+}
+
+function playerMaxHealth(playerNum){
+    const playerMaxHealths = [240, 160, 160, 120, 160,100, 200, 160]
+    return playerMaxHealths[playerNum -1]
+}
+
+function playerHealthBarID(playerNum){
+    const playerHealthBarIDs = ["Player1Health", "Player2Health", "Player3Health", "Player4Health", "Player5Health", "Player6Health", "Player7Health", "Player8Health"]
+    return playerHealthBarIDs[playerNum - 1]
 }
